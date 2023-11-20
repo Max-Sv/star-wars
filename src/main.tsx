b8afcf174d15d72652a406a42ddeccfc890527cc
Max-Sv <mlalx@tut.by>
2023-11-20 10:04:24 +0100

    feat: update tests

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
-import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
-// ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
